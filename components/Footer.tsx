e983644321f4fa837c5f2e3701b3e3b7d5a2f07e
Brady Maki <78286012+bradym05@users.noreply.github.com>
2026-02-15 05:21:21 -0700

    Footer alignment
    Wasn't centered

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -22,10 +22,9 @@ export default function Footer() {
             <Link href="/contract" className="hover:text-gray-900 transition">
               Contract
             </Link>
+            
           </div>
-          <p className="text-sm text-gray-500 mt-4 md:mt-0">
-            © 2026 LifeMap. Made for Calgary Hacks.
-          </p>
+
         </div>
       </div>
     </footer>
